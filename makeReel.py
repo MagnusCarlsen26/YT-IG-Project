9efb62831d4c27d5c9e818e3f0f104eddc811813
MagnusCarlsen26 <khushal.sindhav26@gmail.com>
2024-06-29 13:44:21 +0530

    Automated reels uploading

--- makeReel.py
+++ makeReel.py
@@ -1,23 +1,13 @@
-from background.background import download_background , random_60s_crop
-from scripting.onThisDay import scripting
-from scripting.news import makeNews
+import os
+import requests
+import stable_whisper
 from voiceover.elevenLabs import elevenlabs
 from voiceover.aws_polly import aws_polly
-import moviepy.editor as mp
-import speech_recognition as sr
-from moviepy.editor import VideoFileClip, AudioFileClip, TextClip, CompositeVideoClip
+from moviepy.editor import VideoFileClip, TextClip, CompositeVideoClip
 from moviepy.config import change_settings
 import moviepy.editor as mpe
-import os
-
-
-script = scripting()
-# # script = makeNews('Bollywood news')
-# print(len(script.split()))
-# random_60s_crop('video.mp4')
-# aws_polly(script)
-
-# os.system("ffmpeg -i audio.mp3 audio.wav") 
+from background.background import download_background , random_60s_crop
+from requests_toolbelt.multipart.encoder import MultipartEncoder
 
 IMAGEMAGICK_PATH = r'C:\Program Files\ImageMagick-7.1.1-Q16-HDRI\magick.exe'
 change_settings({'IMAGEMAGICK_BINARY':IMAGEMAGICK_PATH})
@@ -34,11 +24,6 @@ def combine_video_and_audio(video_path, audio_path):
     final_video = video.set_audio(audio)
 
     final_video.write_videofile("Final Video.mp4", codec="libx264", audio_codec="aac")
-
-# combine_video_and_audio("output.mp4", "audio.wav")
-
-import stable_whisper
-from moviepy.editor import VideoFileClip, TextClip, CompositeVideoClip
 
 def generate_captions(video_path, model_size="base"):
     model = stable_whisper.load_model(model_size)
@@ -117,4 +102,82 @@ def generate_captions(video_path, model_size="base"):
     final_video = CompositeVideoClip([video] + captions)
     final_video.write_videofile("output_with_captions.mp4")
 
-# generate_captions("Final Video.mp4")
+def postToIg() :
+    print('Posting to IG ...')
+    response = requests.post('http://localhost:5000/loginNow')
+    try :
+        print(response.text)
+        print(response.text.success)
+    except :
+        pass
+
+    if response.status_code == 200 :
+        print("Logged into IG ...")
+        m = MultipartEncoder(fields={
+            'video': ('Final Video.mp4', open('politicalFacts.mp4', 'rb'), 'video/mp4'),
+            'image': ('image.jpg', open('image.jpg', 'rb'), 'image/jpeg'),
+            'caption' : 'Demo Caption',
+        })
+        response = requests.post('http://localhost:5000/publishVideo', data=m, headers={'Content-Type': m.content_type})
+        print(response.text)
+        print("Posted on IG.")
+        response = requests.post('http://localhost:5000/publishStoryVideo', data=m, headers={'Content-Type': m.content_type})
+        print(response.text)
+
+from scripting.onThisDay import scripting
+from scripting.news import makeNews
+from scripting.motivation.motivation import motivation
+from scripting.worldfacts.wordfacts import worldfacts
+from caption.generateCaption import generateCaption
+
+script = scripting()
+# script = makeNews('Hollywood news')
+# script = motivation()
+# script = worldfacts()
+# caption = generateCaption(script)
+
+# print(f"Number of words = {len(script.split())} ")
+
+aws_regions = [
+    "us-east-2",      # US East (Ohio)
+    "us-east-1",      # US East (N. Virginia)
+    "us-west-1",      # US West (N. California)
+    "us-west-2",      # US West (Oregon)
+    "af-south-1",     # Africa (Cape Town)
+    "ap-east-1",      # Asia Pacific (Hong Kong)
+    "ap-south-1",     # Asia Pacific (Mumbai) 
+    "ap-northeast-1",  # Asia Pacific (Tokyo)
+    "ap-northeast-2",  # Asia Pacific (Seoul)
+    "ap-northeast-3",  # Asia Pacific (Osaka)
+    "ap-southeast-1",  # Asia Pacific (Singapore)
+    "ap-southeast-2",  # Asia Pacific (Sydney)
+    "ap-southeast-3",  # Asia Pacific (Jakarta)
+    "ca-central-1",    # Canada (Central)
+    "eu-central-1",    # Europe (Frankfurt)
+    "eu-west-1",      # Europe (Ireland)
+    "eu-west-2",      # Europe (London)
+    "eu-west-3",      # Europe (Paris)
+    "eu-north-1",     # Europe (Stockholm)
+    "eu-south-1",     # Europe (Milan)
+    "eu-south-2",     # Europe (Spain)
+    "me-south-1",     # Middle East (Bahrain)
+    "sa-east-1",      # South America (São Paulo)
+    # ... (and any newer regions added by AWS)
+]
+random_60s_crop('video.mp4')
+
+for aws_region in aws_regions :
+    try :
+        print(f"Trying {aws_region}")
+        aws_polly(script,aws_region)
+        break
+    except Exception as e:
+        print(f"Failed {e}")
+
+os.system("ffmpeg -i audio.mp3 audio.wav") 
+
+combine_video_and_audio("output.mp4", "audio.wav")
+
+generate_captions("Final Video.mp4")
+
+postToIg()
